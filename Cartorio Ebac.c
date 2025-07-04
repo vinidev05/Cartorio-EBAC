9fff56f4e56a783c9be568880f89c80359acab41
vinidev05 <vini06041013@gmail.com>
2025-07-04 09:51:14 -0300

    Projeto principal
    Começando Cartório EBAC

---
+++ Cartorio Ebac.c
@@ -0,0 +1,50 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //biblioteca de alocação de espaço em memória
+#include <locale.h> ///biblioteca de alocações de texto por região
+
+int main()
+	{
+	int opcao=0; //Definindo variáveis
+	int laco=1;
+	
+	for(laco=1;laco=1;)
+	{
+	    system("cls");
+        
+		setlocale(LC_ALL, "Portuguese"); // Definindo a linguagem
+		
+		printf("### Cartório da EBAC ###\n\n"); //inicio do menu
+		printf("Escolha a opção desejada do menu\n\n");
+		printf("\t1 - Registrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("Opção: "); //fim do menu
+		
+		scanf("%d", &opcao); //armazenamento a escolha do usuário
+		
+		system("cls");
+		
+		switch(opcao)
+		{
+			case 1:
+			printf("Você escolheu o registro de nomes!\n");
+			system("pause");
+			break;	
+
+			case 2:
+			printf("Você escolheu consultar nomes!\n") ;
+			system("pause");
+			break;
+			
+			case 3:
+			printf("Você escolheu deletar nomes!\n");
+			system("pause");
+			break;
+			
+			default:
+			printf("essa opção não está disponivel!\n");
+			system("pause");
+			break;
+		}
+	}
+}
